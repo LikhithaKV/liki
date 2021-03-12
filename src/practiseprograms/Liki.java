7f07f1b1b4d7b40d5c59844e356d40af600ce73d
LikhithaKV <69887017+LikhithaKV@users.noreply.github.com>
2021-03-12 16:35:24 +0530

    Update Liki.java

--- src/practiseprograms/Liki.java
+++ src/practiseprograms/Liki.java
@@ -4,7 +4,8 @@ public class Liki {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("likhitha");
+System.out.println("likhitha shud always be happy");
+		
 	}
 
 }
